de519c7dc67dd7b2d40c0de7fb7fd8481f6221b6
chabermehl <clp.habermehl@gmail.com>
2019-09-05 21:44:17 -0600

    First pass at generating random squares

---
+++ randomSquares.py
@@ -0,0 +1,54 @@
+import turtle
+import random
+
+MAX_CORD = 200
+MIN_CORD = -200
+
+
+def drawSquare(side, t):
+    for i in range(4):
+        t.fd(side)
+        t.rt(90)
+    t.seth(random.randint(0, 360))
+
+
+def moveCursor(x, y, t):
+    t.pu()
+    t.goto(x, y)
+    t.pd()
+
+
+def generateRandoms():
+    randSide = random.randint(0, 301)
+    randX = random.randint(MIN_CORD, MAX_CORD)
+    randY = random.randint(MIN_CORD, MAX_CORD)
+    return randSide, randX, randY
+
+
+def colorPicker():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
+def main():
+    randAmount = random.randint(10, 100)
+    t = turtle.Turtle()
+    t.ht()
+    t.speed(0)
+    turtle.colormode(255)
+    for i in range(randAmount):
+        randoms = generateRandoms()
+        color = colorPicker()
+        t.pencolor(color)
+        t.fillcolor(color)
+        moveCursor(randoms[1], randoms[2], t)
+        t.begin_fill()
+        drawSquare(randoms[0], t)
+        t.end_fill()
+    turtle.done()
+
+
+if __name__ == "__main__":
+    main()
